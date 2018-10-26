0774ad3ed087623639e0982693ad6ebba8cbf117
willem <willem.roos@live.nl>
2018-10-26 13:50:52 +0200

    fix css

--- views/inc/navbar.php
+++ views/inc/navbar.php
@@ -1,5 +1,5 @@
 <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
 <div class="topnav" id="myTopnav">
   <a href="index" class="active">index</a>
